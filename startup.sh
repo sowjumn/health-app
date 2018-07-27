fae131513910f1788f3142d8b5f7aeec259c115d
Jon Byrum <jbyrum@jbyrum-ltm.internal.salesforce.com>
2018-07-26 21:28:56 -0700

    Fixed EC2 instance startup

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ heroku ps:scale web=1 -a health-demo-or
 heroku ps:scale web=1 -a health-demo-to
 heroku ps:scale web=1 -a health-demo-ms-public
 heroku ps:scale worker=1 -a health-demo-ms-private
-
+aws ec2 start-instances --instance-ids i-04dcff2a02c5c1ca5 --region us-west-2
