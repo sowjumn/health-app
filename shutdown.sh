af90506386732b2c31b38958c43bbb970bb70df3
Jon Byrum <jbyrum@jbyrum-ltm.internal.salesforce.com>
2018-08-10 16:19:54 -0700

    Working

--- shutdown.sh
+++ shutdown.sh
@@ -2,6 +2,6 @@ heroku ps:scale web=0 -a health-demo-staging
 heroku ps:scale web=0 -a health-demo-fr
 heroku ps:scale web=0 -a health-demo-or
 heroku ps:scale web=0 -a health-demo-to
-heroku ps:scale web=0 -a health-demo-ms-public
+heroku ps:scale web=0 -a health-demo-ms-private
 heroku ps:scale worker=0 -a health-demo-ms-private
 aws ec2 stop-instances --instance-ids i-04dcff2a02c5c1ca5 --region us-west-2
--- startup.sh
+++ startup.sh
@@ -2,6 +2,6 @@ heroku ps:scale web=1 -a health-demo-staging
 heroku ps:scale web=1 -a health-demo-fr
 heroku ps:scale web=1 -a health-demo-or
 heroku ps:scale web=1 -a health-demo-to
-heroku ps:scale web=1 -a health-demo-ms-public
+heroku ps:scale web=1 -a health-demo-ms-private
 heroku ps:scale worker=1 -a health-demo-ms-private
 aws ec2 start-instances --instance-ids i-04dcff2a02c5c1ca5 --region us-west-2
