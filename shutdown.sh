ae924838b0ca1080850e5fb756bbc23f3b571f76
Jon Byrum <jbyrum@jbyrum-ltm.internal.salesforce.com>
2018-08-21 18:10:46 -0700

    Updated startup and shutdown scripts

--- shutdown.sh
+++ shutdown.sh
@@ -5,3 +5,4 @@ heroku ps:scale web=0 -a health-demo-to
 heroku ps:scale web=0 -a health-demo-risk-staging
 heroku ps:scale web=0 -a health-demo-risk-internal
 aws ec2 stop-instances --instance-ids i-04dcff2a02c5c1ca5 --region us-west-2
+gcloud compute instances stop instance-1 --project byrum-heroku-vpn --zone europe-west3-c
