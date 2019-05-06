e03af157fc70eb4b4961e63f8430e353b13d98d8
Jon Byrum <jbyrum@jbyrum-ltm.internal.salesforce.com>
2019-05-06 11:30:31 -0700

    startup script starts docker container

--- startup.sh
+++ startup.sh
@@ -6,4 +6,11 @@ heroku ps:scale web=2 -a health-demo-risk-staging
 heroku ps:scale web=2 -a health-demo-risk-internal
 aws ec2 start-instances --instance-ids i-04dcff2a02c5c1ca5 --region us-west-2
 gcloud compute instances start instance-1 --project byrum-heroku-vpn --zone europe-west3-c
-echo "Remember to start the docker container on the gcloud instance"
+echo ""
+echo "*** Sleeping for 2 minutes to ensure GCP instance is alive ***"
+sleep 120
+
+echo ""
+echo "*** Starting docker container on GCP instance.  You will have to supply passsword: ***"
+gcloud compute ssh jbyrum@instance-1 --command='. docker-start-app.sh' --project byrum-heroku-vpn --zone europe-west3-c
+
